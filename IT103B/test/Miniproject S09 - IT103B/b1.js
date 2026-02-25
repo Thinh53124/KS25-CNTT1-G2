25fcea5f0c39834be6805647797a77279a119733
Thinh53124 <thinh531240958@gmail.com>
2026-02-25 13:32:59 +0700

    minitest

---
+++ IT103B/test/Miniproject S09 - IT103B/b1.js
@@ -0,0 +1,12 @@
+let orders = ["Đơn hàng A", "Đơn hàng B", "Đơn hàng C", "Đơn hàng D", "Đơn hàng E"];
+let revenues = [1500, 2800, 1200, -500, 3200];
+function orderReports() {
+    `${orders[0]} mang về ${revenues[0]} USD`
+}
+console.log(orderReports());
+
+function sum() {
+    sum.forEach(element => {
+        
+    });
+}
---
+++ IT103B/test/Miniproject S09 - IT103B/b2.js
@@ -0,0 +1,2 @@
+let orders = ["Đơn hàng A", "Đơn hàng B", "Đơn hàng C", "Đơn hàng D", "Đơn hàng E"];
+let revenues = [1500, 2800, 1200, -500, 3200];
