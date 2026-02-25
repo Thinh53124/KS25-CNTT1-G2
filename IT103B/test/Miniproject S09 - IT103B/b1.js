bc28caf6faccfdb04acf4b2d85c96d4d96989f9a
Thinh53124 <thinh531240958@gmail.com>
2026-02-25 13:34:32 +0700

    Update b1.js

--- IT103B/test/Miniproject S09 - IT103B/b1.js
+++ IT103B/test/Miniproject S09 - IT103B/b1.js
@@ -1,12 +1,29 @@
 let orders = ["Đơn hàng A", "Đơn hàng B", "Đơn hàng C", "Đơn hàng D", "Đơn hàng E"];
 let revenues = [1500, 2800, 1200, -500, 3200];
-function orderReports() {
-    `${orders[0]} mang về ${revenues[0]} USD`
-}
-console.log(orderReports());
 
-function sum() {
-    sum.forEach(element => {
-        
+function orderReports() {
+    orders.forEach((order, index) => {
+        console.log(`${order} mang về ${revenues[index]} USD`);
     });
 }
+
+function sumRevenue() {
+    return revenues.reduce((total, value) => total + value, 0);
+}
+
+function positiveRevenue() {
+    return revenues
+        .filter(value => value > 0)
+        .reduce((total, value) => total + value, 0);
+}
+
+function highestOrder() {
+    let max = Math.max(...revenues);
+    let index = revenues.indexOf(max);
+    return `${orders[index]} có doanh thu cao nhất: ${max} USD`;
+}
+
+orderReports();
+console.log("Tổng doanh thu:", sumRevenue());
+console.log("Tổng doanh thu dương:", positiveRevenue());
+console.log(highestOrder());
